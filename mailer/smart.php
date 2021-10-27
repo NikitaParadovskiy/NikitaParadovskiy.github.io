103f6f436f5fb45958d86cb1914038d20cae431f
NikitaParadovskiy <91458095+NikitaParadovskiy@users.noreply.github.com>
2021-10-27 18:44:55 +0300

    Add files via upload

--- mailer/smart.php
+++ mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'baksik11128@gmail.com';                 // Наш логин
-$mail->Password = 'Marina4545';                           // Наш пароль от ящика
+$mail->Password = 'cnbdnoyrgrcozvfm';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
